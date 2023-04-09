7ffe967497c616c2e2403b0134e58f71330493f9
KwangJo Ko <xxjo99@gmail.com>
2023-04-09 18:06:34 +0900

    swagger update

---
+++ src/main/java/com/delivery/mydelivery/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package com.delivery.mydelivery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//@Configuration
+//@EnableSwagger2
+//@EnableAsync
+//@EnableWebMvc
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.delivery.mydelivery")) // Controller가 들어있는 패키지. 이 경로의 하위에 있는 api만 표시됨.
+                .paths(PathSelectors.any()) // 위 패키지 안의 api 중 지정된 path만 보여줌. (any()로 설정 시 모든 api가 보여짐)
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("타이틀")
+                .description("설명부분")
+                .version("1.0.0")
+                .build();
+    }
+
+}
